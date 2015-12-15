1f06dcc2b5d1b079b8f51aed2bf78e43194b6986
Zach Clark <zach@eq>
2015-12-15 11:06:10 -0600

    Initial commit

---
+++ src/x64lisp.lisp
@@ -0,0 +1,18 @@
+
+;;;; A small lisp for writing x64 assembly in C-like style
+;;;
+;;; x64lisp supports at least the following:
+;;; - Functions with variables
+;;; - Arbitrary memory/register access
+;;; - Structs, unions
+;;; - While, for loops
+;;; - Function pointers
+;;; - Basic C integer types (int8/16/32/64)
+;;; - Direct access to assembly instructions
+;;;
+
+(in-package :x64lisp)
+
+(defstruct )
+
+(defmacro )
